144adbf1bfac1e4b5340b4e2aa6af6ec14962e55
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-14 09:54:58 +0000

    Add update and delete endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -20,5 +28,18 @@ export class TasksController {
   @Post()
   async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.taskService.updateTaskStatus(id, status);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.taskService.deleteTask(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -42,4 +42,26 @@ export class TasksService {
 
     return task;
   }
+
+  // update a single task by id
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+
+    if (!task) {
+      throw new NotFoundException('Task with this id not found.');
+    }
+
+    task.status = status;
+    return task;
+  }
+
+  // delete a single task by id
+  deleteTask(id: string): void {
+    const task = this.getTaskById(id);
+    if (!task) {
+      throw new NotFoundException('Task with this id not found.');
+    }
+
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 }
