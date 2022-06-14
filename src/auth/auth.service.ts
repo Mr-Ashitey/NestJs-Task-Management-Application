e0a836a7516e5dd0d45fd1079286ac685f517026
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-14 17:06:56 +0000

    Work on createuser and siginIn endpoints
    - hash password on user creation using bcryptjs
    - create access token on user sign in using jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.usersRepository.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    await this.usersRepository.save(user);
+    const user = this.usersRepository.create({
+      username,
+      password: hashedPassword,
+    });
+
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<object> {
+    const { username, password } = authCredentialsDto;
+
+    const foundUser = await this.usersRepository.findOne({
+      where: { username },
+    });
+
+    if (foundUser && bcrypt.compareSync(password, foundUser.password)) {
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Invalid credentials');
+    }
   }
 }
