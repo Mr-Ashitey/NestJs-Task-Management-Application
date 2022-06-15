87d3219500c765da7e708e812ee728e75cedb597
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-15 14:05:54 +0000

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
@@ -9,6 +10,19 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  const options = new DocumentBuilder()
+    .setTitle('Task Management')
+    .setDescription('Task Management Api')
+    .setVersion('1.0')
+    .addBasicAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'Token' },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   const port = 3000;
   await app.listen(port);
